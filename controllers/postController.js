5d515bf3018e494db12cf907607059735c71ea09
M3LKY <105396866+M3LKY@users.noreply.github.com>
2023-11-16 09:36:15 -0500

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -159,7 +159,7 @@ const getFeedPosts = async (req, res) => {
 };
 
 const getUserPosts = async (req, res) => {
-  const { username } = req.body;
+  const { username } = req.params;
   try {
     const user = await User.findOne({ username });
     if (!user) {
