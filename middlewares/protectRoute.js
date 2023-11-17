05779a76b22bb857015329f7ab64d20995b4ed4f
M3LKY <robinjut10000@gmail.com>
2023-11-17 04:56:07 -0500

    protect

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -2,22 +2,31 @@ import User from "../Schema/userTables.js";
 import jwt from "jsonwebtoken";
 
 const protectRoute = async (req, res, next) => {
-	try {
-		const token = req.cookies.jwt;
+  try {
+    const token = req.cookies.jwt;
 
-		if (!token) return res.status(401).json({ message: "Unauthorized" });
+    if (!token) {
+      console.log("No token found in cookies");
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		const user = await User.findById(decoded.userId).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
 
-		req.user = user;
+    if (!user) {
+      console.log("User not found in the database");
+      return res.status(404).json({ message: "User not found" });
+    }
 
-		next();
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-		console.log("Error in signupUser: ", err.message);
-	}
+    req.user = user;
+
+    console.log("Authentication successful");
+    next();
+  } catch (err) {
+    console.log("Error in protectRoute middleware: ", err.message);
+    res.status(500).json({ message: err.message });
+  }
 };
 
 export default protectRoute;
